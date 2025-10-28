9856022b96bec06f277c82741bc04e068c64ac31
kelvintang <quankelvin04@gmail.com>
2025-10-28 13:11:05 +0100

    Problem2 solved

--- src/main/java/org/example/tasks/Problem2.java
+++ src/main/java/org/example/tasks/Problem2.java
@@ -1,4 +1,43 @@
 package org.example.tasks;
 
+import org.example.readFiles.ReadCsvFiles;
+import java.io.IOException;
+import java.util.List;
+
 public class Problem2 {
+
+    public void sort(double[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            double key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void printArray(double[] arr) {
+        for (double v : arr) {
+            System.out.println(v);
+        }
+    }
+
+    public static void execute() {
+        try {
+            double[] alcoholValues = ReadCsvFiles.getAllAlcoholValues();
+
+            Problem2 sorter = new Problem2();
+            sorter.sort(alcoholValues);
+
+            System.out.println("Sorted alcohol content values (ascending):");
+            printArray(alcoholValues);
+
+        } catch (IOException e) {
+            System.out.println("Error reading wine data: " + e.getMessage());
+        }
+    }
 }
