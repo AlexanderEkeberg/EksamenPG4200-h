8b3ecf0dbf717cf0a44339517a322a538834dce2
AlexanderEkeberg <Alexander@je-design.no>
2025-10-28 14:14:54 +0100

    main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,33 +5,40 @@ import org.example.tasks.Problem1;
 import org.example.tasks.Problem2;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         List<Double> alcoholValues = ReadCsvFiles.getAllAlcoholValues();
-        List<Double> insert = ReadCsvFiles.getAllAlcoholValues();
-        System.out.println("\n=== After Bubble Sort ===");
 
+        List<Double> opt = new ArrayList<>(alcoholValues);
+        List<Double> nonOpt = new ArrayList<>(alcoholValues);
+        List<Double> insert = new ArrayList<>(alcoholValues);
+
+        // Problem 1a
         long start = System.nanoTime();
-        Problem1.bubbleSortNonOptimized(alcoholValues);
+        Problem1.bubbleSortOptimized(opt);
         long end = System.nanoTime();
+        System.out.println("\n=== Optimized Bubble Sort ===");
+        System.out.println(opt);
+        System.out.println("Time: " + (end - start) / 1_000_000.0 + " ms");
 
-        System.out.println(Arrays.toString(alcoholValues.toArray()));
-        double timeMs = (end - start) / 1_000_000.0;
-        System.out.printf("Bubble sort took %.3f ms%n", timeMs);
+        // Problem 1b
+        start = System.nanoTime();
+        Problem1.bubbleSortNonOptimized(nonOpt);
+        end = System.nanoTime();
+        System.out.println("\n=== Non-Optimized Bubble Sort ===");
+        System.out.println(nonOpt);
+        System.out.println("Time: " + (end - start) / 1_000_000.0 + " ms");
 
-        // === Problem 2: Insertion Sort ===
-        System.out.println("\n=== After Insertion Sort ===");
-
-        long start2 = System.nanoTime();
+        // Problem 2
+        start = System.nanoTime();
         Problem2.insertionSort(insert);
-        long end2 = System.nanoTime();
-
-        System.out.println(Arrays.toString(insert.toArray()));
-        double timeMs2 = (end2 - start2) / 1_000_000.0;
-        System.out.printf("Insertion sort took %.3f ms%n", timeMs2);
+        end = System.nanoTime();
+        System.out.println("\n=== Insertion Sort ===");
+        System.out.println(insert);
+        System.out.println("Time: " + (end - start) / 1_000_000.0 + " ms");
 
         // problem 3
 
